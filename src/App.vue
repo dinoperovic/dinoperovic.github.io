e4d97686b8e5c8a4a2a032e7ec198f578a841aae
Dino Perovic <dino.perovic@gmail.com>
2018-01-17 21:50:53 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <h1>Dino Perovic</h1>
-    <p>Hello!</p>
+    <h1>Hi, my name is Dino Perovic.</h1>
+    <p>I'm a Full-Stack Django Web Developer.</p>
+    <p><a href="mailto:dino.perovic@gmail.com">dino.perovic@gmail.com</a></p>
   </div>
 </template>
 
@@ -21,6 +22,22 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 50vh;
+  -webkit-transform: translateY(-50%);
+  transform: translateY(-50%);
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
 }
 </style>
